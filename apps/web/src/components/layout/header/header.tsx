e22ff78da0c1f792f4b556a369aaef70d74f7127
mohammed gehad <mohammed.gehad.1998@gmail.com>
2023-04-09 01:12:37 +0530

    product hunt button

--- apps/web/src/components/layout/header/header.tsx
+++ apps/web/src/components/layout/header/header.tsx
@@ -79,26 +79,44 @@ const Header = () => {
                   </div>
                 </div>
               </div>
-              <a
-                href="https://github.com/mohammed-bahumaish/prisma-editor"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <svg
-                  height="24"
-                  width="24"
-                  aria-hidden="true"
-                  viewBox="0 0 16 16"
-                  version="1.1"
-                  data-view-component="true"
+              <div className="flex items-center gap-2">
+                <a
+                  href="https://www.producthunt.com/posts/prisma-editor?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-prisma&#0045;editor"
+                  target="_blank"
+                  rel="noreferrer"
+                  title="Upvote on Product Hunt"
                 >
-                  <path
-                    fillRule="evenodd"
-                    fill="white"
-                    d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
-                  ></path>
-                </svg>
-              </a>
+                  <img
+                    src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=388147&theme=light"
+                    alt="Prisma&#0032;editor - A&#0032;powerful&#0032;tool&#0032;to&#0032;visualize&#0032;and&#0032;edit&#0032;Prisma&#0032;Schema&#0046; | Product Hunt"
+                    style={{
+                      width: 150,
+                      height: 30,
+                    }}
+                  />
+                </a>
+                <a
+                  href="https://github.com/mohammed-bahumaish/prisma-editor"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  title="Source code on Github"
+                >
+                  <svg
+                    height="24"
+                    width="24"
+                    aria-hidden="true"
+                    viewBox="0 0 16 16"
+                    version="1.1"
+                    data-view-component="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      fill="white"
+                      d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+                    ></path>
+                  </svg>
+                </a>
+              </div>
             </div>
           </div>
           <Disclosure.Panel className="sm:hidden">
