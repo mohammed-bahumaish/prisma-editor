4e279d82660e7f61bf10845d9b35804020e63ccd
mohammed gehad <mohammed.gehad.1998@gmail.com>
2023-04-08 16:35:57 +0530

    google analytics

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import Head from "next/head";
+import Script from "next/script";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -19,6 +20,19 @@ const MyApp: AppType<{ session: Session | null }> = ({
           content="8U9A6jsEwr0vCbYVqJC33MwLSq7YNbk5uRIz8EJdKjs"
         />
       </Head>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-F8EGGW12QB"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-F8EGGW12QB');
+        `}
+      </Script>
       <SessionProvider session={session}>
         <Component {...pageProps} />
       </SessionProvider>
