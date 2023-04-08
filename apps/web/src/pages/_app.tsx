b82d1aa6459535c15cb710d8dfa909b41f532c99
mohammed gehad <mohammed.gehad.1998@gmail.com>
2023-04-08 15:56:59 +0530

    google-site-verification

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -5,15 +5,24 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="8U9A6jsEwr0vCbYVqJC33MwLSq7YNbk5uRIz8EJdKjs"
+        />
+      </Head>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 };
 
