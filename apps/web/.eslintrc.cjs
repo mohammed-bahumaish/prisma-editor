2314746811760b3a1a20a0358c42a421ec734078
mohammed.bahumaish <mohammed.bahumaish@gmail.com>
2025-05-02 01:05:37 +0300

    refactor

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -1,9 +1,23 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["custom"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["next/core-web-vitals"],
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: "./tsconfig.json",
   },
+  rules: {
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-argument": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "@typescript-eslint/restrict-plus-operands": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/require-await": "off",
+    "react-hooks/exhaustive-deps": "warn"
+  }
 };
