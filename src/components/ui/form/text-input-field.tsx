d27c99733534e3bbd1dd6e20932ef74c6bdbc0d0
mohammed.bahumaish <mohammed.bahumaish@gmail.com>
2025-05-02 02:35:38 +0300

    refactor

--- src/components/ui/form/text-input-field.tsx
+++ src/components/ui/form/text-input-field.tsx
@@ -16,19 +16,32 @@ const TextInputField: React.FC<InputProps> = ({ name, label, ...props }) => {
   } = useFormContext();
   const error = errors[name] as FieldError | undefined;
   const errorMessage = error?.message;
+  const [isFocused, setIsFocused] = React.useState(false);
+
+  const handleFocus = () => setIsFocused(true);
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    setIsFocused(false);
+    if (props.onBlur) props.onBlur(e);
+  };
+
+  // Hide error when field is focused
+  const showError = errorMessage && !isFocused;
 
   return (
     <div>
       {label && <Label htmlFor={name}>{label}</Label>}
       <Input
         {...props}
-        {...register(name)}
+        {...register(name, {
+          onBlur: handleBlur,
+        })}
         id={name}
+        onFocus={handleFocus}
         className={clsx({
           "border-red-400 dark:border-red-400": !!errorMessage,
         })}
       />
-      {errorMessage && <p className="text-sm text-red-500">{errorMessage}</p>}
+      {showError && <p className="text-sm text-red-500">{errorMessage}</p>}
     </div>
   );
 };
--- src/server/api/routers/dmmf.ts
+++ src/server/api/routers/dmmf.ts
@@ -4,8 +4,6 @@ import {
   type DMMF,
 } from "@mohammed-bahumaish/prisma-dmmf-extended";
 import { type ConfigMetaFormat } from "@prisma/internals";
-import { execa } from "execa";
-import fs from "fs";
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
@@ -25,33 +23,12 @@ export const dmmfRouter = createTRPCRouter({
           .nullish(),
       })
     )
-    .mutation(
-      async ({ input }) =>
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-        await dmmfToSchema(
-          input as {
-            dmmf: DMMF.Document["datamodel"];
-            config: ConfigMetaFormat;
-          }
-        )
-    ),
+    .mutation(async ({ input }) => {
+      console.log(JSON.stringify(input, null, 2));
+      return await dmmfToSchema(input as any);
+    }),
 
   schemaToDmmf: publicProcedure
     .input(z.string())
     .mutation(async ({ input }) => await schemaToDmmf(input)),
-
-  schemaToSql: publicProcedure.input(z.string()).mutation(async ({ input }) => {
-    const schemafile = "./temp.prisma";
-    fs.writeFileSync(schemafile, input);
-    const { stdout } = await execa("./node_modules/.bin/prisma", [
-      "migrate",
-      "diff",
-      "--to-schema-datamodel",
-      schemafile,
-      "--from-empty",
-      "--script",
-    ]);
-
-    return stdout;
-  }),
 });
