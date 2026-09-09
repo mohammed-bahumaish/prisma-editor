fdd40d489403b8662ac75dcf30b208c35d606d38
dxzfvzs <56274079+dxzfvzs@users.noreply.github.com>
2026-09-09 03:28:32 +0200

    fix: fix incorrect user lookup by email (fixes #111) (#117)
    * fix: fix incorrect user lookup when email is undefined
    
    * refactor: split into two logical auth paths
    
    * refactor: imports as types

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,10 +1,8 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { type GetServerSidePropsContext } from "next";
-import {
-  getServerSession,
-  type DefaultSession,
-  type NextAuthOptions,
-} from "next-auth";
+import { type DefaultSession, getServerSession, type NextAuthOptions, type User } from "next-auth";
+import { type AdapterUser } from "next-auth/adapters";
+import { type JWT } from "next-auth/jwt";
 import GitHubProvider from "next-auth/providers/github";
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
@@ -36,6 +34,34 @@ declare module "next-auth/jwt" {
   }
 }
 
+// user is defined if it comes right from OAuth login, so the info is fresh from gh
+const login = (token: JWT, user: AdapterUser | User) => {
+  token.id = user.id;
+  token.name = user.name;
+  token.email = user.email;
+  token.picture = user.image;
+  return token;
+}
+
+// refresh path with valid (previously authenticated) token
+const refreshToken = async (token: JWT) => {
+  const dbUser = await prisma.user.findUnique({
+    where: { id: token.id },
+  });
+
+  if (!dbUser) {
+    return token;
+  }
+
+  return {
+    ...token,
+    id: dbUser.id,
+    name: dbUser.name,
+    email: dbUser.email,
+    picture: dbUser.image,
+  };
+}
+
 /**
  * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
  *
@@ -56,25 +82,16 @@ export const authOptions: NextAuthOptions = {
 
     // to add `id` to the user session
     async jwt({ token, user }) {
-      const dbUser = await prisma.user.findFirst({
-        where: {
-          email: token.email,
-        },
-      });
+      if (user) {
+        return login(token, user)
+      }
 
-      if (!dbUser) {
-        if (user) {
-          token.id = user?.id;
-        }
+      // malformed token/corrupted state
+      if (!token.id) {
         return token;
       }
 
-      return {
-        id: dbUser.id,
-        name: dbUser.name,
-        email: dbUser.email,
-        picture: dbUser.image,
-      };
+      return (await refreshToken(token));
     },
   },
   session: {
